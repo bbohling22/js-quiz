b057b861bfa3542fea30236f34034dffd9e0ed31
bbohling22 <bbohling22@gmaol.com>
2022-11-21 16:29:10 -0800

    adding new features and updateing code

---
+++ script.js
@@ -0,0 +1,35 @@
+function selectAnswer() {
+
+}
+
+function getNextQuestion() {
+
+}
+
+document.getElementById('countdown').innerHTML =
+  05 + ":" + 00;
+startTimer();
+
+
+function startTimer() {
+  var presentTime = document.getElementById('countdown').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  if(m<0){
+    return
+  }
+  
+  document.getElementById('countdown').innerHTML =
+    m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+  
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
